34d78919ffe80acff01e9b009e2a96846b06ca8f
dnzhelloo <dnzhelloo@icloud.com>
2023-07-05 22:50:52 +0300

    Initial Commit

---
+++ cleanWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cleanWork
+//
+//  Created by Deniz on 5.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
